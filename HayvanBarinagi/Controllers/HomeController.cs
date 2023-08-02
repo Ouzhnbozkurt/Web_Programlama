6b94c3ae1cb0aa1ff74c9842a154fbcf764d1a01
Ouzhnbozkurt <oguz5434@gmail.com>
2023-08-02 23:27:25 +0300

    Hayvan ekleme silme ve düzenleme eklendi.

--- HayvanBarinagi/Controllers/HomeController.cs
+++ HayvanBarinagi/Controllers/HomeController.cs
@@ -24,7 +24,52 @@ namespace HayvanBarinagi.Controllers
             var hayvanListesi = context.Hayvanlar.ToList();
             return View(hayvanListesi);
         }
+        public IActionResult HayvanSil(int Id)
+        {
+            var SecilenHayvan = context.Hayvanlar.Find(Id);
+            context.Hayvanlar.Remove(SecilenHayvan);
+            context.SaveChanges();
 
+            return RedirectToAction("Hayvanlar");
+        }
+        [HttpGet]
+        public IActionResult HayvanEkle()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult HayvanEkle(Hayvan hayvan)
+        {
+            var YeniHayvan = new Hayvan
+            {
+                Adi = hayvan.Adi,
+                Yas = hayvan.Yas,
+                Cins = hayvan.Cins,
+            };
+            context.Hayvanlar.Add(YeniHayvan);
+            context.SaveChanges();
+
+            return RedirectToAction("Hayvanlar");
+        }
+        [HttpGet]
+        public IActionResult HayvanDuzenle(int Id)
+        {
+
+            var SecilenHayvan = context.Hayvanlar.Find(Id);
+            return View(SecilenHayvan);
+        }
+        [HttpPost]
+        public IActionResult HayvanDuzenle(Hayvan hayvan)
+        {
+            var SecilenHayvan = context.Hayvanlar.Find(hayvan.Id);
+            SecilenHayvan.Cins = hayvan.Cins;
+            SecilenHayvan.Adi = hayvan.Adi;
+            SecilenHayvan.Yas = hayvan.Yas;
+            context.SaveChanges();
+
+            return RedirectToAction("Hayvanlar");
+        }
         public IActionResult Sahiplendirme()
         {
             
@@ -35,6 +80,7 @@ namespace HayvanBarinagi.Controllers
 
             return View();
         }
+
         public IActionResult HayvanVerme()
         {
 
